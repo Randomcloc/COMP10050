94ffcfb7b369c5ea5cbb768628b1562e8f4ad957
Abhijeet Suryawanshi <abhijeet@Abhijeets-MacBook-Air.local>
2020-04-27 17:43:13 +0100

    remove pieces/win conditions testing

---
+++ victory.c
@@ -0,0 +1,5 @@
+//
+// Created by Abhijeet Suryawanshi on 27/04/2020.
+//
+
+#include "victory.h"
---
+++ victory.h
@@ -0,0 +1,8 @@
+//
+// Created by Abhijeet Suryawanshi on 27/04/2020.
+//
+
+#ifndef FOCUS_VICTORY_H
+#define FOCUS_VICTORY_H
+
+#endif //FOCUS_VICTORY_H
